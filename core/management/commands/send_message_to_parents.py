f734dfe5130c9be444809cae554e66ae3fe25d2b
Medhat Gayed <medhat.gayed@gmail.com>
2013-08-02 20:20:52 +1200

    Parents message command update.

--- core/management/commands/send_message_to_parents.py
+++ core/management/commands/send_message_to_parents.py
@@ -3,14 +3,16 @@ from core.models import Child
 from django.core import mail
 from django.conf import settings
 from datetime import datetime
-from core.messages import BIRTHDAY
+from core.messages import *
 
 class Command(BaseCommand):
     args = 'child-id child-id ..." or "all"'
     help = "Sends a message to each child's parents."
 
     def handle(self, *args, **options):
-        email_id = 'birthday20130728'
+        email_id = 'trip20130803'
+        message = TRIP_PLAN_CHANGE
+        one_per_parents = True
         
         connection = mail.get_connection()
         connection.open()
@@ -28,16 +30,26 @@ class Command(BaseCommand):
                 except Child.DoesNotExist:
                     raise CommandError('Child "%s" does not exist' % child_id)
         
+        parents_ids = []
         for child in children:
             parents_emails = child.child_parents.get_parents_emails()
             parents_names = child.child_parents.get_parents_names()
-            if parents_emails and not email_id in child.sent_emails:
-                subject = 'Birthday for {0}'.format(child.get_first_name())
-                body = BIRTHDAY.format(parents_names, child.get_first_name())
+            parents_id = child.child_parents.id
+            
+            if parents_id in parents_ids and one_per_parents:
+                if not settings.DEBUG:
+                    child.sent_emails += email_id + ','
+                    child.save()
+                continue
+            elif parents_emails and not email_id in child.sent_emails:
+                parents_ids.append(parents_id)
+                subject = 'Trip plan for tomorrow'
+                body = message.format(parents_names)
                 from_email = 'medhat.gayed@gmail.com'
-                to_emails = parents_emails
-                if settings.DEBUG:
-                    to_emails = ['medhat.gayed@gmail.com',]
+                to_emails = ['medhat.gayed@gmail.com',]
+                
+                if not settings.DEBUG:
+                    to_emails = parents_emails
                 
                 email = mail.EmailMessage(subject,
                                           body,
@@ -45,12 +57,11 @@ class Command(BaseCommand):
                                           to_emails,
                                           connection=connection)
                 email.send()
+                self.stdout.write('Successfully sent email to parents of "%s"\n' % child.name)
                 
                 if not settings.DEBUG:
                     child.sent_emails += email_id + ','
                     child.save()
-                
-                self.stdout.write('Successfully sent email to parents of "%s"\n' % child.name)
             else:
                 self.stdout.write('No email sent for parents of "%s"\n' % child.name)
                 
--- core/messages.py
+++ core/messages.py
@@ -30,3 +30,23 @@ St. Mark's Coptic Orthodox Church, Auckland.
 """
 
 
+TRIP_PLAN_CHANGE = """
+Dear {0},
+
+Peace and Grace,
+
+Due to the rainy weather tomorrow Sunday School will go to Chipmunks albany instead of the zoo.
+
+We will meet at church at 10am then will go to Chipmunks however if you prefer to take your kids
+directly to Chipmunks at 11am feel free to do so.
+
+Chipmunks address is 15 Arrenway Drive, Albany.
+
+Please note Chipmunks won't allow us to bring our own food so the children will need to eat before they go or buy food from the cafe.
+
+We will finish at Chipmunks at 4pm then will go back to church for a shared dinner, we will finish dinner around 6pm.
+
+God bless,
+Sunday School Servants
+St. Mark's Coptic Orthodox Church, Auckland.
+"""
