2a537f5e93c8a8350a7547a4ec3f0099e93f73da
Medhat Gayed <medhat.gayed@gmail.com>
2014-08-19 22:10:20 +1200

    August 2014 birthday message.

--- core/messages.py
+++ core/messages.py
@@ -3,7 +3,7 @@ Dear {0},
 
 Grace and Peace,
 
-Sunday School will celebrate children whose birthday is in July this coming Sunday 27th July.
+Sunday School will celebrate children whose birthday is in August this coming Sunday 24th August.
 
 We would love to have {1} with us, so please make sure you bring {1} to church this Sunday.
 
