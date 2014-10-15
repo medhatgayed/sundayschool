0b39d4dcd8a549b9bf58521a22e43a17ae0126b2
Medhat Gayed <medhat.gayed@yellow.co.nz>
2014-10-15 21:14:45 +1300

    Upgraded to Django 1.7. Upgraded migrations to use built in Django migration system and removed south.

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -1,142 +1,102 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Servant'
-        db.create_table('core_servant', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('dob', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
-            ('phone', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('mobile', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
-            ('address', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-        ))
-        db.send_create_signal('core', ['Servant'])
+    dependencies = [
+    ]
 
-        # Adding model 'ServantYear'
-        db.create_table('core_servantyear', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('servant', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Servant'])),
-            ('year', self.gf('django.db.models.fields.IntegerField')()),
-            ('sunday_school_class', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('book', self.gf('django.db.models.fields.CharField')(max_length=255)),
-        ))
-        db.send_create_signal('core', ['ServantYear'])
-
-        # Adding model 'ServantAttendance'
-        db.create_table('core_servantattendance', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('servant', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Servant'])),
-            ('date', self.gf('django.db.models.fields.DateField')()),
-            ('attended_mass', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('attended_meeting', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('attended_sunday_school', self.gf('django.db.models.fields.BooleanField')(default=False)),
-        ))
-        db.send_create_signal('core', ['ServantAttendance'])
-
-        # Adding model 'Child'
-        db.create_table('core_child', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('dob', self.gf('django.db.models.fields.DateField')(null=True, blank=True)),
-            ('phone', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('father_name', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('father_mobile', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('father_email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
-            ('mother_name', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('mother_mobile', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('mother_email', self.gf('django.db.models.fields.EmailField')(max_length=75, blank=True)),
-            ('address', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-        ))
-        db.send_create_signal('core', ['Child'])
-
-        # Adding model 'ChildYear'
-        db.create_table('core_childyear', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('child', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['core.Child'])),
-            ('year', self.gf('django.db.models.fields.IntegerField')()),
-            ('sunday_school_class', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('book', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('school_year', self.gf('django.db.models.fields.IntegerField')()),
-        ))
-        db.send_create_signal('core', ['ChildYear'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Servant'
-        db.delete_table('core_servant')
-
-        # Deleting model 'ServantYear'
-        db.delete_table('core_servantyear')
-
-        # Deleting model 'ServantAttendance'
-        db.delete_table('core_servantattendance')
-
-        # Deleting model 'Child'
-        db.delete_table('core_child')
-
-        # Deleting model 'ChildYear'
-        db.delete_table('core_childyear')
-
-
-    models = {
-        'core.child': {
-            'Meta': {'object_name': 'Child'},
-            'address': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'dob': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'father_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'father_mobile': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'father_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mother_email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'mother_mobile': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'mother_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'phone': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
-        },
-        'core.childyear': {
-            'Meta': {'object_name': 'ChildYear'},
-            'book': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'child': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Child']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'school_year': ('django.db.models.fields.IntegerField', [], {}),
-            'sunday_school_class': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'year': ('django.db.models.fields.IntegerField', [], {})
-        },
-        'core.servant': {
-            'Meta': {'object_name': 'Servant'},
-            'address': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'dob': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
-            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mobile': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'phone': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
-        },
-        'core.servantattendance': {
-            'Meta': {'object_name': 'ServantAttendance'},
-            'attended_mass': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'attended_meeting': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'attended_sunday_school': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'date': ('django.db.models.fields.DateField', [], {}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'servant': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Servant']"})
-        },
-        'core.servantyear': {
-            'Meta': {'object_name': 'ServantYear'},
-            'book': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'servant': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Servant']"}),
-            'sunday_school_class': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'year': ('django.db.models.fields.IntegerField', [], {})
-        }
-    }
-
-    complete_apps = ['core']
+    operations = [
+        migrations.CreateModel(
+            name='Child',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('dob', models.DateField(null=True, blank=True)),
+                ('school_year', models.IntegerField(null=True, blank=True)),
+                ('sent_emails', models.TextField(blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ChildParents',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('phone', models.CharField(max_length=255, blank=True)),
+                ('father_name', models.CharField(max_length=255, blank=True)),
+                ('father_mobile', models.CharField(max_length=255, blank=True)),
+                ('father_email', models.EmailField(max_length=75, blank=True)),
+                ('mother_name', models.CharField(max_length=255, blank=True)),
+                ('mother_mobile', models.CharField(max_length=255, blank=True)),
+                ('mother_email', models.EmailField(max_length=75, blank=True)),
+                ('address', models.CharField(max_length=255, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Servant',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('uid', models.CharField(max_length=255)),
+                ('dob', models.DateField(null=True, blank=True)),
+                ('phone', models.CharField(max_length=255, blank=True)),
+                ('mobile', models.CharField(max_length=255, blank=True)),
+                ('email', models.EmailField(max_length=75, blank=True)),
+                ('address', models.CharField(max_length=255, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='ServantAttendance',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date', models.DateField()),
+                ('attended_mass', models.BooleanField()),
+                ('attended_meeting', models.BooleanField()),
+                ('attended_sunday_school', models.BooleanField()),
+                ('servant', models.ForeignKey(to='core.Servant')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='SundaySchoolClass',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, blank=True)),
+                ('book', models.CharField(max_length=255, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='servant',
+            name='sunday_school_class',
+            field=models.ForeignKey(blank=True, to='core.SundaySchoolClass', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='child',
+            name='child_parents',
+            field=models.ForeignKey(blank=True, to='core.ChildParents', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='child',
+            name='sunday_school_class',
+            field=models.ForeignKey(blank=True, to='core.SundaySchoolClass', null=True),
+            preserve_default=True,
+        ),
+    ]
