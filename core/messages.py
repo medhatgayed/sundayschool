b8a86bf162da67fa1da69c571866c058af732cbc
Medhat Gayed <medhat.gayed@gmail.com>
2015-04-15 19:28:23 +1200

    March 2015 birthday message.

--- core/messages.py
+++ core/messages.py
@@ -3,7 +3,7 @@ Dear {0},
 
 Grace and Peace,
 
-Sunday School will celebrate children whose birthday is in February this coming Sunday 15th February.
+Sunday School will celebrate children whose birthday is in March this coming Sunday 15th March.
 
 We would love to have {1} with us! So please make sure you bring {1} to church this coming Sunday.
 
