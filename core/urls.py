cb168241af177e314629abdd86d709816be50962
Medhat Gayed <medhat.gayed@gmail.com>
2016-01-29 20:56:37 +1300

    Added a new view to show children going to year 7.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-from core.views import class_list, altar_list
+from core.views import class_list, altar_list, children_going_to_year_7
 
 urlpatterns = [
     url(r'^class/(?P<name>\d+&?\d?)/', class_list),
     url(r'^altar-list/', altar_list),
+    url(r'^children-going-to-year-7/', children_going_to_year_7),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@
 from core.models import Child, SundaySchoolClass
 from django.shortcuts import render
 
+
 def class_list(request, name):
     sunday_school_class = SundaySchoolClass.objects.get(name=name)
     return render(request, 'core/class_list.html', {'sunday_school_class': sunday_school_class})
@@ -10,3 +11,8 @@ def class_list(request, name):
 def altar_list(request):
     sunday_school_classes = SundaySchoolClass.objects.all()
     return render(request, 'core/altar_list.html', {'sunday_school_classes': sunday_school_classes})
+
+
+def children_going_to_year_7(request):
+    children = Child.objects.filter(school_year=6, is_active=True)
+    return render(request, 'core/children_going_to_year_7.html', {'children': children})
