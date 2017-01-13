535a2c6f4aa367c5b8250076dbe5ab5baa33d8ef
Medhat Gayed <medhat.gayed@gmail.com>
2017-01-13 19:22:49 +1300

    Modified command to use new argument parser. Added the option to pass the birthday month as an argument.

--- core/management/commands/send_message_to_parents.py
+++ core/management/commands/send_message_to_parents.py
@@ -6,8 +6,24 @@ from datetime import datetime
 from core.messages import *
 
 class Command(BaseCommand):
-    args = 'child-id child-id ..." or "all"'
-    help = "Sends a message to each child's parents."
+    help = "Sends an email to children's parents."
+
+    def add_arguments(self, parser):
+        parser.add_argument('--all',
+            action='store_true',
+            dest='all',
+            default=False,
+            help='Send email to all parents.')
+        parser.add_argument('--children-ids',
+            action='store',
+            dest='children_ids',
+            default=False,
+            help='Send email only to parents who have children with these ids (comma separated list of ids).')
+        parser.add_argument('--birthday-month',
+            action='store',
+            dest='birthday_month',
+            default=datetime.now().month,
+            help='Send email only to parents who have children with a birthday on that month.')
 
     def handle(self, *args, **options):
         email_id = settings.MESSAGE_ID
@@ -15,32 +31,34 @@ class Command(BaseCommand):
         one_per_parents = settings.MESSAGE_ONE_PER_PARENTS
         is_sample = settings.MESSAGE_IS_SAMPLE
         sample_count = settings.MESSAGE_SAMPLE_COUNT
-        is_birthday = False
-        
+        is_all = options.get('all')
+        birthday_month = options.get('birthday_month')
+        is_birthday = True if birthday_month else False
+        children_ids = options.get('children_ids')
+
         connection = mail.get_connection()
         connection.open()
-        
-        if args[0] == 'all':
+
+        if is_all:
             children = Child.objects.filter(is_active=True)
-        elif args[0] == 'birthday':
-            is_birthday = True
-            children = Child.objects.filter(dob__month=datetime.now().month, is_active=True)
-        else:
+        elif birthday_month:
+            children = Child.objects.filter(dob__month=int(birthday_month), is_active=True)
+        elif children_ids:
             children = []
-            for child_id in args:
+            for child_id in children_ids.split(','):
                 try:
                     child = Child.objects.get(pk=int(child_id))
                     children.append(child)
                 except Child.DoesNotExist:
                     raise CommandError('Child "%s" does not exist' % child_id)
-        
+
         parents_ids = []
         count = 0
         for child in children:
             parents_emails = child.child_parents.get_parents_emails()
             parents_names = child.child_parents.get_parents_names()
             parents_id = child.child_parents.id
-            
+
             if parents_id in parents_ids and one_per_parents:
                 if not is_sample:
                     child.sent_emails += email_id + ','
@@ -55,10 +73,10 @@ class Command(BaseCommand):
                     body = message.format(parents_names, child.get_first_name())
                 from_email = settings.MESSAGE_FROM_EMAIL
                 to_emails = settings.MESSAGE_TO_EMAILS
-                
+
                 if not is_sample:
                     to_emails = parents_emails
-                
+
                 email = mail.EmailMessage(subject,
                                           body,
                                           from_email,
@@ -69,7 +87,7 @@ class Command(BaseCommand):
                     email.send()
                     count += 1
                 self.stdout.write('Successfully sent email to parents of "%s"\n' % child.name)
-                
+
                 if not is_sample:
                     child.sent_emails += email_id + ','
                     child.save()
@@ -78,5 +96,5 @@ class Command(BaseCommand):
 
             if is_sample and count >= sample_count:
                 break
-                
+
         connection.close()
