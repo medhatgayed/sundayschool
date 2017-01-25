4f9821555520f170f56890f9ab07a3f450a8b3d7
Medhat Gayed <medhat.gayed@gmail.com>
2017-01-25 22:10:21 +1300

    Remove email sending from command, only use command to export csv file of children details.

--- core/management/commands/send_children_details_to_servants.py
+++ core/management/commands/send_children_details_to_servants.py
@@ -6,15 +6,11 @@ import csv
 from datetime import datetime
 
 class Command(BaseCommand):
-    help = "Sends each class children details to class servants."
+    help = "Export a CSV file of children details for each Sunday School class."
 
     def handle(self, *args, **options):
-        email_id = 'cts20130413'
-        
-        connection = mail.get_connection()
-        connection.open()
-        
         for sunday_school_class in SundaySchoolClass.objects.all():
+            self.stdout.write('Exporting children details for year {0}\n'.format(sunday_school_class.name))
             csv_file_path = '/tmp/children-details-for-year-{0}.csv'.format(sunday_school_class.name)
             with open(csv_file_path, 'w') as f:
                 w = csv.writer(f, quoting=csv.QUOTE_ALL)
@@ -30,7 +26,7 @@ class Command(BaseCommand):
                             'Mother Mobile',
                             'Mother Email',
                             'Home Address',])
-                children = sunday_school_class.child_set.all()
+                children = sunday_school_class.child_set.filter(is_active=True)
                 for child in children:
                     child_parents = child.child_parents
                     w.writerow([str(child.name),
@@ -45,36 +41,3 @@ class Command(BaseCommand):
                                 str(child_parents.mother_mobile),
                                 str(child_parents.mother_email),
                                 str(child_parents.address),])
-        
-            subject = 'Updated class details for year {0}'.format(sunday_school_class.name)
-            body = """
-     +
-Dear Servants,
-
-Peace and Grace,
-
-Please find attached the updated details for your class.
-
-If you have more details that are not in the attached file please email them to me and I will update the data.
-
-God bless,
-Sunday School Servants"""
-            from_email = 'medhat.gayed@gmail.com'
-            to_emails = sunday_school_class.get_servants_emails()
-            cc_emails = ['gorgyhanna@yahoo.co.nz','medhat.gayed@gmail.com',]
-            if settings.DEBUG:
-                to_emails = ['medhat.gayed@gmail.com',]
-                cc_emails = ['medhat.gayed@gmail.com',]
-            
-            email = mail.EmailMessage(subject,
-                                      body,
-                                      from_email,
-                                      to_emails,
-                                      cc=cc_emails,
-                                      connection=connection)
-            email.attach_file(csv_file_path)
-            email.send()
-            
-            self.stdout.write('Successfully sent email to servants of year {0}\n'.format(sunday_school_class.name))
-                
-        connection.close()
